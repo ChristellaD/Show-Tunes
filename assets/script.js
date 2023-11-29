1bf9db795c44698077c10b550b70e7b4cb63de93
Nathan Dickinson <nathandickinson04@gamil.com>
2023-11-28 20:34:18 -0500

    Added a function to get the search history and create buttons and made the input work for searching

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,56 @@
 var youtubeKey = 'AIzaSyA174a2gY04tvmL5EB1EP4iGnyHr4mVkOg';
 var youtubeEl = $('.youtube');
 var submitBtn = $('#submitBtn');
+var userInput = $('#userInput');
+var artist = [];
+contHistEl = $('.history')
 
 submitBtn.on("click", function (event) {
   event.preventDefault();
   
+  userInput = $(this).siblings('#userInput').val();
+	if (userInput === '') {
+		return;
+	};
+
+  artist.push(userInput);
+
+	localStorage.setItem('artist', JSON.stringify(artist)); 
+
   getYoutube();
+  getHistory();
 });
+
+getHistory();
+
+
+function getHistory() {
+  var getartist = JSON.parse(localStorage.getItem('artist'));
+  
+  $(contHistEl).empty();
+ 
+	for (let i = 0; i < getartist.length; i++) {
+
+		var rowEl = document.createElement('div');
+		var btnEl = document.createElement('button');
+    btnEl.classList = 'histBtn';
+    btnEl.textContent = (getartist[i].toUpperCase());
+		contHistEl.append(rowEl);
+		rowEl.append(btnEl);
+	} if (!artist) {
+		return;
+	}
+
+	$('.histBtn').on("click", function (event) {
+		userInput = $(this).text();
+    getYoutube();
+	});
+};
 
 function getYoutube() {
 youtubeEl.empty()
-    var youtubeUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=radiohead&type=video&key=AIzaSyA174a2gY04tvmL5EB1EP4iGnyHr4mVkOg'
-    
+    var youtubeUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q='+ userInput +'&type=video&key=AIzaSyA174a2gY04tvmL5EB1EP4iGnyHr4mVkOg'
+    console.log(youtubeUrl)
     fetch(youtubeUrl)
     .then(function (response) {
         return response.json();
@@ -32,6 +71,19 @@ youtubeEl.empty()
             thumbnailEl.append(title);
           };
       });
-
 }
 
+function getSpotify() {//Needs token 
+  var clientId = 'ed507333f63b4e30a1828dea0595685a'
+  var clientSecret = '15662e91b9ae4f658b35ab168e4b344e'
+  var spotifyUrl = 'https://api.spotify.com/v1/search/?query=' + userInput + '&type=album&locale=en-US%2Cen%3Bq%3D0.9&offset=0&limit=5'
+  fetch(spotifyUrl)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data){
+        console.log(data)
+      });
+}
+
+getSpotify()
