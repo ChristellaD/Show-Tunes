316db60f963a4451441f0c9072b68f1c9153a4ae
nathan26036 <nathandickinson04@gmail.com>
2023-11-30 19:38:00 -0500

    Adding spotify redirect

--- assets/script.js
+++ assets/script.js
@@ -73,11 +73,88 @@ youtubeEl.empty()
       });
 }
 
-function getSpotify() {//Needs token 
-  var clientId = 'ed507333f63b4e30a1828dea0595685a'
-  var clientSecret = '15662e91b9ae4f658b35ab168e4b344e'
+function getSpotify() {
+
+  const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+  
+  const codeVerifier  = generateRandomString(64);
+  
+  const sha256 = async (plain) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+  }
+  
+const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+  }
+  
+  const hashed = sha256(codeVerifier)
+  const codeChallenge = base64encode(hashed);
+  
+
+const clientId = 'ed507333f63b4e30a1828dea0595685a'
+const redirectUri = 'https://christellad.github.io/Show-Tunes/';
+
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: clientId,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+authUrl.search = new URLSearchParams(params).toString();
+window.location.href = authUrl.toString();
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+const getToken = async code => {
+
+  // stored in the previous step
+  let codeVerifier = localStorage.getItem('code_verifier');
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application   /x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const response =await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+  
+}
+
+  let accessToken = localStorage.getItem('access_token');
   var spotifyUrl = 'https://api.spotify.com/v1/search/?query=' + userInput + '&type=album&locale=en-US%2Cen%3Bq%3D0.9&offset=0&limit=5'
-  fetch(spotifyUrl)
+  fetch(spotifyUrl, {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  })
     .then(function (response) {
         return response.json();
       })
